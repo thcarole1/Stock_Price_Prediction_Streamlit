518c3ccda0a8a95a08f458089213120d4ccec158
Thierry CAROLE <thcarole1@hotmail.com>
2024-10-09 23:44:19 +0200

    spinner ok

--- app/home.py
+++ app/home.py
@@ -7,13 +7,9 @@ import numpy as np
 import pandas as pd
 import shutil
 
-# Initialize the presence of a ticker to False
+
 if 'ticker_entered' not in st.session_state:
     st.session_state['ticker_entered'] = False
-
-# Initialize predicting phase to False
-if 'prediction_done' not in st.session_state:
-    st.session_state['prediction_done'] = False
 
 # *********  Erase and create folder  **************************************
 def delete_create_folder(folder_name):
@@ -37,9 +33,7 @@ def prepare():
     # ***************************************************************************
     print('delete and create are done')
 
-    # Set the presence of a ticker to True
     st.session_state['ticker_entered'] = True
-    # print(st.session_state)
 # ***************************************************************************
 
 st.markdown(
@@ -75,64 +69,67 @@ ticker = st.text_input("Please enter a valid stock ticker :",
               on_change=prepare,
               placeholder="AAPL, MSFT, STLA for example")
 
-if st.session_state['ticker_entered']:
+def perform_function():
     st.write(f"The chosen ticker is {ticker}.")
 
-# ************************** Connection to API ***************************************
-# API endpoint
-URL = "https://stockpriceprediction-yyzifgu2zq-od.a.run.app/ticker/"
+    # ************************** Connection to API ***************************************
+    # API endpoint
+    URL = "https://stockpriceprediction-yyzifgu2zq-od.a.run.app/ticker/"
 
-# Sending POST request and saving the response as a response object
-r = requests.get(url=URL, params={'query': ticker})
+    # Sending POST request and saving the response as a response object
+    r = requests.get(url=URL, params={'query': ticker})
 
-# st.write(r.status_code)
+    # st.write(r.status_code)
 
-# Check if the response contains a ZIP file
-if r.status_code == 200 and r.headers['Content-Type'] == 'application/zip':
-    # Save the ZIP file
-    with open("raw_data/output.zip", "wb") as file:
-        file.write(r.content)
-    print("ZIP file downloaded successfully!")
-else:
-    print("Failed to download the ZIP file or incorrect response.")
+    # Check if the response contains a ZIP file
+    if r.status_code == 200 and r.headers['Content-Type'] == 'application/zip':
+        # Save the ZIP file
+        with open("raw_data/output.zip", "wb") as file:
+            file.write(r.content)
+        print("ZIP file downloaded successfully!")
+    else:
+        print("Failed to download the ZIP file or incorrect response.")
 
-# Open the ZIP file
-with zipfile.ZipFile('raw_data/output.zip', 'r') as zip_ref:
-    # List all files within the ZIP
-    file_list = zip_ref.namelist()
-    # print("Files in the ZIP:", file_list)
+    # Open the ZIP file
+    with zipfile.ZipFile('raw_data/output.zip', 'r') as zip_ref:
+        # List all files within the ZIP
+        file_list = zip_ref.namelist()
+        # print("Files in the ZIP:", file_list)
 
-    # Extract all files to the current directory (optional)
-    zip_ref.extractall('extracted_data')
-# **************************************************************************
+        # Extract all files to the current directory (optional)
+        zip_ref.extractall('extracted_data')
+    # **************************************************************************
 
-# ******************** Display images ***********************************
-# Load your images
-train_test_pred_image = Image.open("extracted_data/data/processed_data/train_test_pred.png")
-test_pred_image = Image.open("extracted_data/data/processed_data/test_pred.png")
-test_pred_limited_image = Image.open("extracted_data/data/processed_data/test_pred_limited.png")
+    # ******************** Display images ***********************************
+    # Load your images
+    train_test_pred_image = Image.open("extracted_data/data/processed_data/train_test_pred.png")
+    test_pred_image = Image.open("extracted_data/data/processed_data/test_pred.png")
+    test_pred_limited_image = Image.open("extracted_data/data/processed_data/test_pred_limited.png")
 
-# Display the image in Streamlit
-st.image(train_test_pred_image,
-        caption='train_test_pred_image',
-        use_column_width=True)
+    # Display the image in Streamlit
+    st.image(train_test_pred_image,
+            caption='train_test_pred_image',
+            use_column_width=True)
 
-st.image(test_pred_image,
-        caption='test_pred_image',
-        use_column_width=True)
+    st.image(test_pred_image,
+            caption='test_pred_image',
+            use_column_width=True)
 
-st.image(test_pred_limited_image,
-        caption='test_pred_limited_image',
-        use_column_width=True)
+    st.image(test_pred_limited_image,
+            caption='test_pred_limited_image',
+            use_column_width=True)
+    # **************************************************************************
 
-# **************************************************************************
+    # **************** Display dataframe ***************************************
+    path_to_csv = "extracted_data/summary.csv"
+    summary = pd.read_csv(path_to_csv)
+    summary = summary.set_index(keys = 'index')
+    st.write(summary)
+    # **************************************************************************
+    st.session_state['ticker_entered'] = False
 
-# **************** Display dataframe ***************************************
-path_to_csv = "extracted_data/summary.csv"
-summary = pd.read_csv(path_to_csv)
-summary = summary.set_index(keys = 'index')
-st.write(summary)
 
-# **************************************************************************
-
-st.write("The end !!!")
+# Display a loading message while the calculation is in progress
+if st.session_state['ticker_entered']:
+    with st.spinner('Calculating...'):
+        perform_function()
