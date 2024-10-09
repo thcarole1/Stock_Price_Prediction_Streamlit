8cfe8db53ad5f5073804029839128489c2fb97d1
Thierry CAROLE <thcarole1@hotmail.com>
2024-10-09 18:01:06 +0200

    csv_to_df display ok

--- app/home.py
+++ app/home.py
@@ -90,31 +90,32 @@ import shutil
 #     zip_ref.extractall('extracted_data')
 # # **************************************************************************
 
-# # ******************** Display images ***********************************
-# # Load your images
-# train_test_pred_image = Image.open("extracted_data/data/processed_data/train_test_pred.png")
-# test_pred_image = Image.open("extracted_data/data/processed_data/test_pred.png")
-# test_pred_limited_image = Image.open("extracted_data/data/processed_data/test_pred_limited.png")
+# ******************** Display images ***********************************
+# Load your images
+train_test_pred_image = Image.open("extracted_data/data/processed_data/train_test_pred.png")
+test_pred_image = Image.open("extracted_data/data/processed_data/test_pred.png")
+test_pred_limited_image = Image.open("extracted_data/data/processed_data/test_pred_limited.png")
 
-# # Display the image in Streamlit
-# st.image(train_test_pred_image,
-#          caption='train_test_pred_image',
-#          use_column_width=True)
+# Display the image in Streamlit
+st.image(train_test_pred_image,
+         caption='train_test_pred_image',
+         use_column_width=True)
 
-# st.image(test_pred_image,
-#          caption='test_pred_image',
-#          use_column_width=True)
+st.image(test_pred_image,
+         caption='test_pred_image',
+         use_column_width=True)
 
-# st.image(test_pred_limited_image,
-#          caption='test_pred_limited_image',
-#          use_column_width=True)
+st.image(test_pred_limited_image,
+         caption='test_pred_limited_image',
+         use_column_width=True)
 
-# # **************************************************************************
+# **************************************************************************
 
 
 # **************** Display dataframe ***************************************
 path_to_csv = "extracted_data/summary.csv"
 summary = pd.read_csv(path_to_csv)
+summary = summary.set_index(keys = 'index')
 st.write(summary)
 
 # **************************************************************************
